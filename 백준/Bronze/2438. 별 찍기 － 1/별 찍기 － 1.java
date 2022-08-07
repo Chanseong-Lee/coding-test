c2d8c780bfb91183e1819ff80a8b984158a92ef8
Chanseong Lee <94969452+Chanseong-Lee@users.noreply.github.com>
2022-08-07 15:29:11 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 196 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int line = scan.nextInt();
+		
+		for(int i = 1; i <= line; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
